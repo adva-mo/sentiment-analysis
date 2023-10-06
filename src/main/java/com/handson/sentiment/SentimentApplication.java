8bec2c2381c70a2a143cd5817eb3b1e8a5f2faaa
adva-mo <advamozes@gmail.com>
2023-10-06 14:18:07 +0300

    with nlp

--- src/main/java/com/handson/sentiment/SentimentApplication.java
+++ src/main/java/com/handson/sentiment/SentimentApplication.java
@@ -1,4 +1,4 @@
-package com.example.sentiment;
+package com.handson.sentiment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/handson/sentiment/SentimentApplicationTests.java
+++ src/test/java/com/handson/sentiment/SentimentApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.sentiment;
+package com.handson.sentiment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
